67660a44ec4e740a1bdc3bbfb758a5536b3cb644
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-01-21 09:49:14 -0600

    captured users names and print on the screen

---
+++ app.js
@@ -0,0 +1,7 @@
+let playerX = prompt("What is your name? \nYou will be X");
+let playerY = prompt("What is your name? \nYou will be Y");
+
+document.write(`${playerX}, you are X! You go first.`);
+document.write(`${playerY}, you are Y!`);
+
+
